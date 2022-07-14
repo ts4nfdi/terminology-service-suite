1f9d6fc378b5646c2cf77af8e8e4e11d3e3869ec
zimmermann <masterp4085>
2022-07-14 17:20:13 +0200

    added MetaDataWidget

---
+++ src/components/widgets/MetadataWidget/MetadataWidget.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { OntologyHierarchyWidget } from './OntologyHierarchyWidget';
+import { IriWidget } from './IriWidget';
+import { TermWidget } from './TermWidget';
+import { DescriptionWidget } from './DescriptionWidget';
+import { TabWidget } from './TabWidget';
+
+interface MetadataWidgetProps {
+  iri: string;
+  api:string;
+}
+
+function MetadataWidget(props: MetadataWidgetProps) {
+  return (
+    <EuiFlexGroup direction="column" style={{ maxWidth: 600 }}>
+      <EuiFlexItem grow={false}>
+        <span>
+          <OntologyHierarchyWidget iri={props.iri} api={props.api} />
+        </span>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiFlexGroup direction="column">
+          <EuiFlexItem>
+            <EuiFlexGroup>
+              <EuiFlexItem grow={false}>
+                <IriWidget iri={props.iri} api={props.api} />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+
+          <EuiFlexItem grow={false}>
+            <TermWidget iri={props.iri} api={props.api} />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <DescriptionWidget iri={props.iri} api={props.api} />
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <TabWidget iri={props.iri} api={props.api} />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+export { MetadataWidget };
