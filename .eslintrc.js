e93e142d31dee6a94fa2ea352251b5ca87417a57
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-08-10 15:43:09 +0200

    added eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jsx-a11y"],
+  root: true,
+};
