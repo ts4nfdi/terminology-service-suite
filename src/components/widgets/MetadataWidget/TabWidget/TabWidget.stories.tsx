89b435874c816213ef7878490286d7fbc17d077f
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-08-19 10:57:37 +0200

    added controls, args to story

--- src/components/widgets/MetadataWidget/TabWidget/TabWidget.stories.tsx
+++ src/components/widgets/MetadataWidget/TabWidget/TabWidget.stories.tsx
@@ -1,17 +1,34 @@
 import React from "react";
-import { TabWidget } from "./TabWidget";
+import { TabWidget, TabWidgetProps } from "./TabWidget";
 
 export default {
   title: "TabWidget",
-  component: TabWidget
+  component: TabWidget,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    api: {
+      description: "API",
+      control: {
+        type: "radio",
+        options: [
+          "https://www.ebi.ac.uk/ols/api/",
+          "https://semanticlookup.zbmed.de/ols/api/",
+        ],
+      },
+    },
+  },
 };
 
+const Template = (args: TabWidgetProps) => <TabWidget {...args} />;
 
-export const TabWidget1 = () => (
-           <TabWidget   api={"https://semanticlookup.zbmed.de/ols/api/"}
-                        iri={"http://purl.obolibrary.org/obo/NCIT_C2985"}
-                        linkToSelf={"https://semanticlookup.zbmed.de/ols/api/ontologies/mesh/terms/"}/>
-);
+export const TabWidget1 = Template.bind({});
 
-
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+TabWidget1.args = {
+  api: "https://semanticlookup.zbmed.de/ols/api/",
+  iri: "http://purl.obolibrary.org/obo/NCIT_C2985",
+  linkToSelf: "https://semanticlookup.zbmed.de/ols/api/ontologies/ncit/terms/",
+};
