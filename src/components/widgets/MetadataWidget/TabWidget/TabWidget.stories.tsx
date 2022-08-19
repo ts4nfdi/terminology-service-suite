0d58cc133810e8c92f8fe732d6bcf99a1667c511
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-08-19 09:33:17 +0200

    deleted argTypes

--- src/components/widgets/MetadataWidget/TabWidget/TabWidget.stories.tsx
+++ src/components/widgets/MetadataWidget/TabWidget/TabWidget.stories.tsx
@@ -1,35 +1,17 @@
 import React from "react";
-import { TabWidget, TabWidgetProps } from "./TabWidget";
+import { TabWidget } from "./TabWidget";
 
 export default {
-  title: "/Widgets/TabWidget",
-  component: TabWidget,
-  parameters: {
-    layout: "centered",
-  },
-  argTypes: {
-    api: {
-      description: "API",
-      control: {
-        type: "radio",
-        options: [
-          "https://www.ebi.ac.uk/ols/api/",
-          "https://semanticlookup.zbmed.de/ols/api/",
-          "http://localhost:8080/api/",
-          "http://localhost:5000/api/",
-        ],
-      },
-    },
-  },
+  title: "TabWidget",
+  component: TabWidget
 };
 
-const Template = (args: TabWidgetProps) => <TabWidget {...args} />;
 
-export const TabWidget1 = Template.bind({});
+export const TabWidget1 = () => (
+           <TabWidget   api={"https://semanticlookup.zbmed.de/ols/api/"}
+                        iri={"http://purl.obolibrary.org/obo/NCIT_C2985"}
+                        linkToSelf={"https://semanticlookup.zbmed.de/ols/api/ontologies/mesh/terms/"}/>
+);
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-TabWidget1.args = {
-  iri: "http://purl.obolibrary.org/obo/NCIT_C2985",
-  api: "https://semanticlookup.zbmed.de/ols/api/",
-};
+
+
