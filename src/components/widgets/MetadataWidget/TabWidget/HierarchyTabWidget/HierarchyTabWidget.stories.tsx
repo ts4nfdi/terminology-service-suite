043d5ad5bab72ac696315cbe478c8b20e989166d
Pierre Zimmermann <pierre.zimmermann@smail.inf.h-brs.de>
2022-08-19 10:57:51 +0200

    added controls, args to story

--- src/components/widgets/MetadataWidget/TabWidget/HierarchyTabWidget/HierarchyTabWidget.stories.tsx
+++ src/components/widgets/MetadataWidget/TabWidget/HierarchyTabWidget/HierarchyTabWidget.stories.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-
-import { HierarchyTabWidget } from "./HierarchyTabWidget";
+import { HierarchyTabIF, HierarchyTabWidget } from "./HierarchyTabWidget";
 
 export default {
   title: "HierarchyTabWidget",
   component: HierarchyTabWidget,
+  argTypes: {
+    api: {
+      description: "API",
+      control: {
+        type: "radio",
+        options: [
+          "https://www.ebi.ac.uk/ols/api/",
+          "https://semanticlookup.zbmed.de/ols/api/",
+        ],
+      },
+    },
+  },
 };
 
-export const HierarchyTabWidget1 = () => (
-      <HierarchyTabWidget
-        iri={"http://purl.bioontology.org/ontology/MESH/D003704"}
-        linkToSelf={
-          "https://semanticlookup.zbmed.de/ols/api/ontologies/mesh/terms/"
-        }
-      />
-);
+const Template = (args: HierarchyTabIF) => <HierarchyTabWidget {...args} />;
+
+export const HierarchyTabWidget1 = Template.bind({});
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+HierarchyTabWidget1.args = {
+  iri: "http://purl.bioontology.org/ontology/MESH/D003704",
+  api: "https://semanticlookup.zbmed.de/ols/api/",
+  linkToSelf: "https://semanticlookup.zbmed.de/ols/api/ontologies/mesh/terms/",
+};
