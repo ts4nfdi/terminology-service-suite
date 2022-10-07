7bab254e189b57a38aa3f5c83e773a388d0b5b7c
Vera Clemens <clemens@zbmed.de>
2022-10-07 18:38:31 +0200

    feat: add number of terms, properties and individuals to data content widget

--- src/components/widgets/DataContentWidget/DataContentWidget.stories.tsx
+++ src/components/widgets/DataContentWidget/DataContentWidget.stories.tsx
@@ -2,35 +2,35 @@ import React from "react";
 import { DataContentWidget, DataContentWidgetProps } from "./DataContentWidget";
 
 export default {
-    title: "DataContentWidget",
-    component: DataContentWidget,
-    parameters: {
-        layout: "centered",
+  title: "DataContentWidget",
+  component: DataContentWidget,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    api: {
+      description: "Instance of the OLS API to call.",
+      control: {
+        type: "radio",
+        options: [
+          "https://www.ebi.ac.uk/ols/api/",
+          "https://semanticlookup.zbmed.de/ols/api/",
+        ],
+      },
     },
-    argTypes: {
-        api: {
-            description: "Instance of the OLS API to call.",
-            control: {
-                type: "radio",
-                options: [
-                    "https://www.ebi.ac.uk/ols/api/",
-                    "https://semanticlookup.zbmed.de/ols/api/",
-                ],
-            },
-        },
-    },
+  },
 };
 
 const Template = (args: DataContentWidgetProps) => (
-    <>
-        <DataContentWidget {...args} />
-    </>
+  <>
+    <DataContentWidget {...args} />
+  </>
 );
 
 export const DataContentWidget1 = Template.bind({});
 
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 DataContentWidget1.args = {
-    api: "https://semanticlookup.zbmed.de/ols/api/",
+  api: "https://semanticlookup.zbmed.de/ols/api/",
 };
--- src/components/widgets/DataContentWidget/DataContentWidget.tsx
+++ src/components/widgets/DataContentWidget/DataContentWidget.tsx
@@ -1,62 +1,140 @@
 import React, { useEffect, useState } from "react";
 import { EuiText, EuiLoadingSpinner, EuiCard } from "@elastic/eui";
-import { EuiTextProps } from "@elastic/eui/src/components/text/text";
 import { useQuery } from 'react-query'
 
 export interface DataContentWidgetProps {
-    api: string;
+  api: string;
 }
 
+const NOT_AVAILABLE = "n/a";
+
+const getTotalElements = (url: string) => {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Content_Type: "application/json",
+    },
+  })
+  .then(response => response.json())
+  .then(response => {
+    if (response.page.totalElements != null) {
+      return response.page.totalElements;
+    } else {
+      return NOT_AVAILABLE;
+    }
+  });
+};
+
 function DataContentWidget(props: DataContentWidgetProps) {
-    const { api, ...rest } = props;
-    const [totalOntologies, setTotalOntologies] = useState("Not available")
+  const { api, ...rest } = props;
+  const [totalOntologies, setTotalOntologies] = useState<string | number>(NOT_AVAILABLE);
+  const [totalTerms, setTotalTerms] = useState<string | number>(NOT_AVAILABLE);
+  const [totalProperties, setTotalProperties] = useState<string | number>(NOT_AVAILABLE);
+  const [totalIndividuals, setTotalIndividuals] = useState<string | number>(NOT_AVAILABLE);
 
-    const {
-        data: ontologiesData,
-        isSuccess: isSuccessOntologies,
-        isLoading: isLoadingOntologies,
-        isError: isErrorOntologies
-    } = useQuery(
-        ["ontologiesData", api],
-        () => {
-            return fetch(`${api}ontologies`, {
-                method: "GET",
-                headers: {
-                    Accept: "application/json",
-                    Content_Type: "application/json",
-                },
-            }).then(response => response.json())
-                .then(response => {
-                    if (response.page.totalElements != null) {
-                        return response.page.totalElements;
-                    } else {
-                        return "Not available"
-                    }
-                });
-        }
-    )
-
-    useEffect(() => {
-        if (isSuccessOntologies) {
-            setTotalOntologies(ontologiesData)
-        }
-    }, [ontologiesData])
+  useEffect(() => {
+    // Reset to initial state when API changes
+    setTotalOntologies(NOT_AVAILABLE);
+    setTotalTerms(NOT_AVAILABLE);
+    setTotalProperties(NOT_AVAILABLE);
+    setTotalIndividuals(NOT_AVAILABLE);
+  }, [api]);
 
 
-    return (
-        <>
-            <EuiCard title={"Data Content"}>
-                <EuiText {...rest}>
-                    <ul>
-                        {isSuccessOntologies && <li>{totalOntologies} ontologies and terminologies</li>}
-                        {isLoadingOntologies && <EuiLoadingSpinner/>}
-                        {isErrorOntologies && <li>An error occured</li>}
-                    </ul>
+  /* TODO Remove code duplication? */
 
-                </EuiText>
-            </EuiCard>
-        </>
-    );
+  const {
+    data: ontologiesData,
+    isSuccess: isSuccessOntologies,
+    isLoading: isLoadingOntologies,
+    dataUpdatedAt: dataUpdatedAt
+  } = useQuery(
+    ["ontologiesData", api],
+    () => getTotalElements(`${api}ontologies?size=1`),
+    { refetchOnWindowFocus: false }
+  );
+
+  useEffect(() => {
+    if (isSuccessOntologies) {
+      setTotalOntologies(ontologiesData);
+    }
+  }, [ontologiesData]);
+
+
+  const {
+    data: termsData,
+    isSuccess: isSuccessTerms,
+    isLoading: isLoadingTerms
+  } = useQuery(
+    ["termsData", api],
+    () => getTotalElements(`${api}terms?size=1`),
+    { refetchOnWindowFocus: false }
+  );
+
+  useEffect(() => {
+    if (isSuccessTerms) {
+      setTotalTerms(termsData);
+    }
+  }, [termsData]);
+
+
+  const {
+    data: propertiesData,
+    isSuccess: isSuccessProperties,
+    isLoading: isLoadingProperties
+  } = useQuery(
+    ["propertiesData", api],
+    () => getTotalElements(`${api}properties?size=1`),
+    { refetchOnWindowFocus: false }
+  );
+
+  useEffect(() => {
+    if (isSuccessProperties) {
+      setTotalProperties(propertiesData);
+    }
+  }, [propertiesData]);
+
+
+  const {
+    data: individualsData,
+    isSuccess: isSuccessIndividuals,
+    isLoading: isLoadingIndividuals,
+  } = useQuery(
+    ["individualsData", api],
+    () => getTotalElements(`${api}individuals?size=1`),
+    { refetchOnWindowFocus: false }
+  );
+
+  useEffect(() => {
+    if (isSuccessIndividuals) {
+      setTotalIndividuals(individualsData);
+    }
+  }, [individualsData]);
+
+
+  return (
+    <>
+      <EuiCard
+        title="Data Content"
+        description={dataUpdatedAt ? `Updated ${new Date(dataUpdatedAt).toLocaleString()}` : ''}
+        layout="horizontal"
+      >
+        <EuiText {...rest}>
+          {isLoadingOntologies || isLoadingTerms || isLoadingProperties || isLoadingIndividuals
+            ? <EuiLoadingSpinner/>
+            : <ul>
+                <li>{totalOntologies.toLocaleString()} ontologies and terminologies</li>
+                <li>{totalTerms.toLocaleString()} terms</li>
+                <li>{totalProperties.toLocaleString()} properties</li>
+                <li>{totalIndividuals.toLocaleString()} individuals</li>
+                <li>Version n/a</li> {/* TODO how to get version? */}
+              </ul>
+          }
+        </EuiText>
+      </EuiCard>
+    </>
+  );
 }
 
 export { DataContentWidget };
