91b5f2397ef7a9036d104defd32864cd66130a8e
Vera Clemens <clemens@zbmed.de>
2022-10-11 17:33:33 +0200

    refactor: use less hooks in data content widget

--- src/components/widgets/DataContentWidget/DataContentWidget.tsx
+++ src/components/widgets/DataContentWidget/DataContentWidget.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { EuiText, EuiLoadingSpinner, EuiCard } from "@elastic/eui";
-import { useQuery } from 'react-query'
+import { useQuery, QueryFunction } from 'react-query';
 
 export interface DataContentWidgetProps {
   api: string;
 }
 
 const NOT_AVAILABLE = "n/a";
+const QUERY_STALE_TIME = 5 * 60 * 1000;
 
-const getTotalElements = (url: string) => {
-  return fetch(url, {
+const getTotalElements: QueryFunction<number, [string]> = ({queryKey}) => {
+  const [queryPath] = queryKey;
+
+  return fetch(queryPath, {
     method: "GET",
     headers: {
       Accept: "application/json",
@@ -21,51 +24,34 @@ const getTotalElements = (url: string) => {
     if (response.page.totalElements != null) {
       return response.page.totalElements;
     } else {
-      return NOT_AVAILABLE;
+      throw new Error("page.totalElements is null in API response");
     }
   });
 };
 
-const useFetchTotalFromAPI = (queryPath: string, setTotal: React.Dispatch<React.SetStateAction<string | number>>): [boolean, number] => {
-  const {
-    data: data,
-    isSuccess: isSuccess,
-    isLoading: isLoading,
-    dataUpdatedAt: dataUpdatedAt
-  } = useQuery(
-    [queryPath],
-    () => getTotalElements(queryPath),
-    { refetchOnWindowFocus: false }
-  );
-
-  useEffect(() => {
-    if (isSuccess) {
-      setTotal(data);
-    }
-  }, [data]);
-
-  return [isLoading, dataUpdatedAt];
-};
-
 function DataContentWidget(props: DataContentWidgetProps) {
   const { api, ...rest } = props;
-  const [totalOntologies, setTotalOntologies] = useState<string | number>(NOT_AVAILABLE);
-  const [totalTerms, setTotalTerms] = useState<string | number>(NOT_AVAILABLE);
-  const [totalProperties, setTotalProperties] = useState<string | number>(NOT_AVAILABLE);
-  const [totalIndividuals, setTotalIndividuals] = useState<string | number>(NOT_AVAILABLE);
 
-  useEffect(() => {
-    // Reset to initial state when API changes
-    setTotalOntologies(NOT_AVAILABLE);
-    setTotalTerms(NOT_AVAILABLE);
-    setTotalProperties(NOT_AVAILABLE);
-    setTotalIndividuals(NOT_AVAILABLE);
-  }, [api]);
+  const {
+    data: totalOntologies,
+    isLoading: isLoadingOntologies,
+    dataUpdatedAt: dataUpdatedAtOntologies
+  } = useQuery([`${props.api}ontologies?size=1`], getTotalElements, { staleTime: QUERY_STALE_TIME });
 
-  const [isLoadingOntologies, dataUpdatedAtOntologies] = useFetchTotalFromAPI(`${props.api}ontologies?size=1`, setTotalOntologies);
-  const [isLoadingTerms,] = useFetchTotalFromAPI(`${props.api}terms?size=1`, setTotalTerms);
-  const [isLoadingProperties,] = useFetchTotalFromAPI(`${props.api}properties?size=1`, setTotalProperties);
-  const [isLoadingIndividuals,] = useFetchTotalFromAPI(`${props.api}individuals?size=1`, setTotalIndividuals);
+  const {
+    data: totalTerms,
+    isLoading: isLoadingTerms
+  } = useQuery([`${props.api}terms?size=1`], getTotalElements, { staleTime: QUERY_STALE_TIME });
+
+  const {
+    data: totalProperties,
+    isLoading: isLoadingProperties
+  } = useQuery([`${props.api}properties?size=1`], getTotalElements, { staleTime: QUERY_STALE_TIME });
+
+  const {
+    data: totalIndividuals,
+    isLoading: isLoadingIndividuals
+  } = useQuery([`${props.api}individuals?size=1`], getTotalElements, { staleTime: QUERY_STALE_TIME });
 
   return (
     <>
@@ -76,11 +62,11 @@ function DataContentWidget(props: DataContentWidgetProps) {
       >
         <EuiText {...rest}>
           <ul>
-            <li>{isLoadingOntologies ? <EuiLoadingSpinner size="s" /> : totalOntologies.toLocaleString()} ontologies and terminologies</li>
-            <li>{isLoadingTerms ? <EuiLoadingSpinner size="s" /> : totalTerms.toLocaleString()} terms</li>
-            <li>{isLoadingProperties ? <EuiLoadingSpinner size="s" /> : totalProperties.toLocaleString()} properties</li>
-            <li>{isLoadingIndividuals ? <EuiLoadingSpinner size="s" /> : totalIndividuals.toLocaleString()} individuals</li>
-            <li>Version n/a</li> {/* TODO how to get version? */}
+            <li>{isLoadingOntologies ? <EuiLoadingSpinner size="s" /> : (totalOntologies ? totalOntologies.toLocaleString() : NOT_AVAILABLE)} ontologies and terminologies</li>
+            <li>{isLoadingTerms ? <EuiLoadingSpinner size="s" /> : (totalTerms ? totalTerms.toLocaleString() : NOT_AVAILABLE)} terms</li>
+            <li>{isLoadingProperties ? <EuiLoadingSpinner size="s" /> : (totalProperties ? totalProperties.toLocaleString() : NOT_AVAILABLE)} properties</li>
+            <li>{isLoadingIndividuals ? <EuiLoadingSpinner size="s" /> : (totalIndividuals ? totalIndividuals.toLocaleString() : NOT_AVAILABLE)} individuals</li>
+            <li>Version {NOT_AVAILABLE}</li> {/* TODO how to get version? */}
           </ul>
         </EuiText>
       </EuiCard>
