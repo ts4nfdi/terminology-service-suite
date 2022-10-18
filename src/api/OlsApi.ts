ed0085d2aaf219b082a40f6dce98cbc4388c5796
Vera Clemens <clemens@zbmed.de>
2022-10-18 17:36:41 +0200

    feat(OlsApi): add function for /search

--- src/api/OlsApi.ts
+++ src/api/OlsApi.ts
@@ -20,6 +20,17 @@ interface ContentParams {
 
 export type apiCallFn = (paginationParams?: PaginationParams, sortingParams?: SortingParams, contentParams?: ContentParams) => Promise<any>;
 
+interface SearchQueryParams {
+  query: string;
+  exactMatch?: boolean;
+  showObsoleteTerms?: boolean;
+  types?: string;
+  ontology?: string;
+  groupByIri?: boolean;
+}
+
+const DEFAULT_SEARCH_RESULTS_PER_PAGE = 10;
+
 export class OlsApi {
   private axiosInstance: AxiosInstance;
 
@@ -33,27 +44,62 @@ export class OlsApi {
     });
   }
 
-  private buildParams(paginationParams?: PaginationParams, sortingParams?: SortingParams) {
+  private buildParamsForGet(paginationParams?: PaginationParams, sortingParams?: SortingParams) {
     if (sortingParams) {
       return { ...paginationParams, sort: `${sortingParams.sortField},${sortingParams.sortDir}` };
     }
     return { ...paginationParams };
   }
 
+  private buildParamsForSearch(queryParams: SearchQueryParams, paginationParams: PaginationParams) {
+    const params: any = {
+      q: queryParams.query,
+      rows: paginationParams.size,
+      exact: queryParams.exactMatch,
+      obsoletes: queryParams.showObsoleteTerms,
+    };
+
+    if (queryParams.groupByIri) {
+      params.groupField = queryParams.groupByIri;
+    }
+
+    if (queryParams.types) {
+      params.type = queryParams.types;
+    }
+
+    if (queryParams.ontology) {
+      params.ontology = queryParams.ontology;
+    }
+  
+    if (paginationParams.page) {
+      if (paginationParams.size) {
+        params.start = (+paginationParams.page * +paginationParams.size).toString();
+      } else {
+        params.start = (+paginationParams.page * DEFAULT_SEARCH_RESULTS_PER_PAGE).toString();
+      }
+    }
+  
+    return params;
+  }
+
   public getOntologies: apiCallFn = async (paginationParams, sortingParams) => {
-    return (await this.axiosInstance.get("ontologies", { params: this.buildParams(paginationParams, sortingParams) })).data;
+    return (await this.axiosInstance.get("ontologies", { params: this.buildParamsForGet(paginationParams, sortingParams) })).data;
   }
 
   public getTerms: apiCallFn = async (paginationParams, sortingParams) => {
-    return (await this.axiosInstance.get("terms", { params: this.buildParams(paginationParams, sortingParams) })).data;
+    return (await this.axiosInstance.get("terms", { params: this.buildParamsForGet(paginationParams, sortingParams) })).data;
   }
 
   public getProperties: apiCallFn = async (paginationParams, sortingParams) => {
-    return (await this.axiosInstance.get("properties", { params: this.buildParams(paginationParams, sortingParams) })).data;
+    return (await this.axiosInstance.get("properties", { params: this.buildParamsForGet(paginationParams, sortingParams) })).data;
   }
 
   public getIndividuals: apiCallFn = async (paginationParams, sortingParams) => {
-    return (await this.axiosInstance.get("individuals", { params: this.buildParams(paginationParams, sortingParams) })).data;
+    return (await this.axiosInstance.get("individuals", { params: this.buildParamsForGet(paginationParams, sortingParams) })).data;
+  }
+
+  public search = async (queryParams: SearchQueryParams, paginationParams: PaginationParams): Promise<any> => {
+    return (await this.axiosInstance.get("search", { params: this.buildParamsForSearch(queryParams, paginationParams) })).data;
   }
 
   public getOntology: apiCallFn = async (paginationParams, sortingParams, contentParams) => {
