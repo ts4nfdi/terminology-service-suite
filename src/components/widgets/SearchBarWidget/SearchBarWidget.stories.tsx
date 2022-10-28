ba6b831d51e761e182c27ffd3021652b3e67f713
Vera Clemens <clemens@zbmed.de>
2022-10-28 14:14:26 +0200

    feat: add new search bar widget with autosuggest using /suggest

---
+++ src/components/widgets/SearchBarWidget/SearchBarWidget.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { SearchBarWidget, SearchBarWidgetProps } from "./SearchBarWidget";
+
+export default {
+  title: "SearchBarWidget",
+  component: SearchBarWidget,
+  argTypes: {
+    api: {
+      description: "Instance of the OLS API to call.",
+      control: {
+        type: "radio",
+        options: [
+          "https://www.ebi.ac.uk/ols/api/",
+          "https://semanticlookup.zbmed.de/ols/api/",
+        ],
+      },
+    },
+    query: {
+      description: "The search term to receive suggestions for.",
+    },
+    onSearchValueChange: {
+      description: "Function to be called when the search value in the search bar changes.",
+      action: "onSearchValueChange"
+    }
+  },
+};
+
+const Template = (args: SearchBarWidgetProps) => (
+  <>
+    <SearchBarWidget {...args} />
+  </>
+);
+
+export const SearchBarWidget1 = Template.bind({});
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+SearchBarWidget1.args = {
+  api: "https://semanticlookup.zbmed.de/ols/api/",
+  query: "diab",
+};
