7a0f8d53fd59315144b3e6d8ae21a720a9eaf779
sasse <sasse@zbmed.de>
2023-03-17 14:25:15 +0100

    Revert "feat: add storybook versions"
    This reverts commit ef470232f030c2f3e0d0f454119f8c1b9c8a46d4.

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@panosvoudouris/addon-versions/register"
   ],
   framework: "@storybook/react",
 };
