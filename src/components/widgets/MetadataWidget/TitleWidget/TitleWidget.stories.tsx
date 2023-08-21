79e07e25449678bb0f3be69ebc8df3b8aca142a0
darms <darms@zbmed.de>
2023-08-21 15:36:22 +0200

    feature(TitleWidget): Allow users to pass default value to the component that is displayed if no API response is available.

--- src/components/widgets/MetadataWidget/TitleWidget/TitleWidget.stories.tsx
+++ src/components/widgets/MetadataWidget/TitleWidget/TitleWidget.stories.tsx
@@ -1,66 +1,67 @@
 import React from "react";
-import { TitleWidget, TitleWidgetProps } from "./TitleWidget";
-import { EuiPanel } from "@elastic/eui";
+import {TitleWidget, TitleWidgetProps} from "./TitleWidget";
+import {EuiPanel} from "@elastic/eui";
 
 export default {
-  title: "TitleWidget",
-  component: "TitleWidget",
-  parameters: {
-    layout: "centered",
-  },
-  argTypes: {
-    api: {
-      description: "Instance of the OLS API to call.",
-      control: {
-        type: "radio",
-        options: [
-          "https://www.ebi.ac.uk/ols/api/",
-          "https://semanticlookup.zbmed.de/ols/api/",
-          "https://semanticlookup.zbmed.de/api/",
-        ],
-      },
+    title: "TitleWidget",
+    component: TitleWidget,
+    parameters: {
+        layout: "centered",
     },
-    ontologyId: {
-      description: "Ontology ID from where the object title/label should be taken.",
+    argTypes: {
+        api: {
+            description: "Instance of the OLS API to call.",
+            control: {
+                type: "radio",
+                options: [
+                    "https://www.ebi.ac.uk/ols/api/",
+                    "https://semanticlookup.zbmed.de/ols/api/",
+                    "https://semanticlookup.zbmed.de/api/",
+                ],
+            },
+        },
+        ontologyId: {
+            description: "Ontology ID from where the object title/label should be taken.",
+        },
+        entityType: {
+            description: "Sets the type of the object whose title/label you want to fetch. Accepts 'ontology', 'term', 'class', 'property', or 'individual'.",
+            control: {
+                type: "radio",
+                options: [
+                    "ontology",
+                    "term",
+                    "class",
+                    "property",
+                    "individual",
+                    "INVALID STRING"
+                ],
+            },
+        },
+        iri: {
+            description: "Object IRI whose label you want to fetch. For ontologies this is ignored, since the 'ontologyId' arg is sufficient.",
+        },
+        titleText: {
+            description:
+                "Set your own text manually that overwrites the text fetched from the API",
+        },
+        default_value: {
+            control: 'text',
+            description:
+                "Set the default text shown if no API fails to retrieve one.",
+        },
+        parameter: {
+            defaultValue: "collection=nfdi4health",
+            type: {required: false}
+        },
     },
-    entityType: {
-      description: "Sets the type of the object whose title/label you want to fetch. Accepts 'ontology', 'term', 'class', 'property', or 'individual'.",
-      control: {
-        type: "radio",
-        options: [
-          "ontology",
-          "term",
-          "class",
-          "property",
-          "individual",
-          "INVALID STRING"
-        ],
-      },
-    },
-    iri: {
-      description: "Object IRI whose label you want to fetch. For ontologies this is ignored, since the 'ontologyId' arg is sufficient.",
-    },
-    titleText: {
-      description:
-        "Set your own text manually that overwrites the text fetched from the API",
-    },
-    default_value:{
-        description:
-        "Set the default text shown if no API fails to retrieve one.",
-    },
-    parameter: {
-      defaultValue: "collection=nfdi4health",
-      type: { required: false }
-    },
-  },
 };
 
 const Template = (args: TitleWidgetProps) => (
-  <>
-    <EuiPanel>
-      <TitleWidget {...args} />
-    </EuiPanel>
-  </>
+    <>
+        <EuiPanel>
+            <TitleWidget {...args} />
+        </EuiPanel>
+    </>
 );
 
 export const TitleWidget1 = Template.bind({});
@@ -68,7 +69,7 @@ export const TitleWidget1 = Template.bind({});
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 TitleWidget1.args = {
-  iri: "http://purl.obolibrary.org/obo/NCIT_C2985",  api: "https://semanticlookup.zbmed.de/api/",
-  ontologyId: "ncit",
-  entityType: "term",
+    iri: "http://purl.obolibrary.org/obo/NCIT_C2985", api: "https://semanticlookup.zbmed.de/api/",
+    ontologyId: "ncit",
+    entityType: "term",
 };
