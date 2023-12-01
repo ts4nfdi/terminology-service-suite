022100738c4da5d9f5ec1176d4b2c3ad6403f332
Roman Baum <baum@zbmed.de>
2023-12-01 15:22:23 +0100

    chore(fix): updated the API from OLS to OLS4.

--- src/components/widgets/ResourcesWidget/ResourcesWidget.stories.tsx
+++ src/components/widgets/ResourcesWidget/ResourcesWidget.stories.tsx
@@ -20,7 +20,7 @@ export default {
         type: "radio",
       },
       options: [
-        "https://www.ebi.ac.uk/ols/api/",
+        "https://www.ebi.ac.uk/ols4/api",
         "https://semanticlookup.zbmed.de/ols/api/",
         "https://semanticlookup.zbmed.de/api/",
       ],
