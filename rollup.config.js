a10f9ffb151dca05652b4eb9a320baed2cc859c0
sasse <sasse@zbmed.de>
2024-01-10 10:49:04 +0100

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
+import css from 'rollup-plugin-css-only';
 
 export default [
   {
     input: "src/index.ts",
-    external: ["@elastic/eui", "react", "react-query"],
+    external: ["@elastic/eui", "react", "react-query", "@emotion/react", "@zbmed/ols-treeview", "axios"],
     output: {
       dir: "dist/esm",
       format: "esm",
       sourcemap: true,
     },
-    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" }), css({ output: 'bundle.css' })],
   },
 ];
