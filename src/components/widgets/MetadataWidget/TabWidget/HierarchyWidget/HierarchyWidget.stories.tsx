c07aed362185f83820de9dbef73af3f02f784e1c
Vincent Kneip <kneip@zbmed.de>
2024-02-27 18:10:40 +0100

    fix(HierarchyWidget Storybook): removed unused queries from story file, provided entityType

--- src/components/widgets/MetadataWidget/TabWidget/HierarchyWidget/HierarchyWidget.stories.tsx
+++ src/components/widgets/MetadataWidget/TabWidget/HierarchyWidget/HierarchyWidget.stories.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { HierarchyWidgetProps, HierarchyWidget } from "./HierarchyWidget";
-import { Entity, Ontology, Thing } from "../../../../../model/interfaces";
-import { useQuery } from "react-query";
-import { OlsApi } from "../../../../../api/OlsApi";
 
 export default {
   title: "HierarchyWidget",
@@ -26,6 +23,15 @@ export default {
     ontologyId: {
       description: "Ontology ID from where the term hierarchy should be taken.",
     },
+    entityType: {
+      control: { type: "radio" },
+      options: [
+          "properties",
+          "individuals",
+          "classes",
+          ""
+      ]
+    },
     iri: {
       description: "Iri of the term you want to fetch the term hierarchy for.",
     },
@@ -38,46 +44,6 @@ export default {
   }
 };
 
-const iri = "http://www.ebi.ac.uk/efo/EFO_0000400"
-const api = "http://ols4.qa.km.k8s.zbmed.de/ols4/api/"
-const ontologyId = "efo"
-const entityType = "term"
-const parameter = "collection=nfdi4health"
-const useLegacy = false
-const olsApi = new OlsApi(api);
-
-function getData(){
-  const {
-    data,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useQuery<Thing>(
-    ["metadata", api, parameter, entityType, iri, ontologyId, useLegacy],
-    async () => {
-      return olsApi.getResponseObject(entityType, iri, ontologyId, parameter, useLegacy);
-    }
-  );
-}
-
-function getOntologyData(){
-  const {
-    data: ontologyData,
-    isLoading: isLoadingOntology,
-    isSuccess: isSuccessOntology,
-    isError: isErrorOntology,
-    error: errorOntology
-  } = useQuery<Thing>(
-    ["ontologyData", api, parameter, entityType, iri, ontologyId, useLegacy],
-    async () => {
-      return olsApi.getResponseObject("ontology", iri, ontologyId, parameter, useLegacy);
-    }
-  );
-}
-
-
-
 const Template = (args: HierarchyWidgetProps) => <HierarchyWidget {...args} />;
 
 export const HierarchyWidget1 = Template.bind({});
@@ -85,8 +51,9 @@ export const HierarchyWidget1 = Template.bind({});
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 HierarchyWidget1.args = {
-  iri: "http://www.ebi.ac.uk/efo/EFO_0000400",
+  iri: "http://www.ebi.ac.uk/efo/EFO_0000741",
   api: "http://ols4.qa.km.k8s.zbmed.de/ols4/api/",
   ontologyId: "efo",
-  parameter: "collection=nfdi4health",
+  entityType: "properties",
+  //parameter: "collection=nfdi4health",
 };
