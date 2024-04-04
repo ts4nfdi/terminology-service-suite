364629b4a82bd36ae934f6b9fa7256ef3c9e6bc5
sasse <sasse@zbmed.de>
2024-04-04 11:00:59 +0200

    feat(ci): build react and html storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,16 +16,31 @@ module.exports = {
   typescript: {
     reactDocgen: 'react-docgen'
   },
-  refs: {
-    react: {
-      title: "REACT",
-      // this has to be the port on which storybook:react runs
-      url: "http://localhost:6006",
-    },
-    html: {
-      title: "HTML",
-      // this has to be the port on which storybook:html runs
-      url: "http://localhost:6007",
+  refs: (config, { configType }) => {
+    const envConfigType = process.env.CONFIG_TYPE || configType;
+    if (envConfigType === 'DEVELOPMENT') {
+      return {
+        react: {
+          title: 'REACT',
+          // this has to be the port on which storybook:react runs
+          url: 'http://localhost:6006',
+        },
+        angular: {
+          title: 'HTML',
+          // this has to be the port on which storybook:html runs
+          url: 'http://localhost:6007',
+        },
+      };
     }
+    return {
+      react: {
+        title: 'REACT',
+        url: 'https://nfdi4health.github.io/semlookp-widgets/react/latest',
+      },
+      angular: {
+        title: 'HTML',
+        url: 'https://nfdi4health.github.io/semlookp-widgets/html/latest',
+      },
+    };
   },
 };
