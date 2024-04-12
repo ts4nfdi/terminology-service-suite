7a1b613a3619fea6fbee20f8c95dd3014f29d488
sasse <sasse@zbmed.de>
2024-04-12 11:16:18 +0200

    fix(AutocompleteWidget): show actions

--- src/components/widgets/AutocompleteWidget/AutocompleteWidgetStories.ts
+++ src/components/widgets/AutocompleteWidget/AutocompleteWidgetStories.ts
@@ -28,6 +28,18 @@ export const AutocompleteWidgetStoryArgTypes = {
   }
 }
 
+export const AutocompleteWidgetStoryArgsReact = {
+  args: {
+    api: "https://semanticlookup.zbmed.de/ols/api/",
+    parameter: "ontology=mesh,efo&type=class&collection=nfdi4health&fieldList=description,label,iri,ontology_name,type,short_form",
+    hasShortSelectedLabel: true,
+    allowCustomTerms: false,
+    singleSelection: true,
+    placeholder: "Search for a Concept",
+    preselected: [],
+  },
+};
+
 export const AutocompleteWidgetStoryArgs = {
   args: {
     api: "https://semanticlookup.zbmed.de/ols/api/",
