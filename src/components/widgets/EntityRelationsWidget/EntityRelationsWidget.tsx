6b3d7b1c05233590fda06f6d130ed1d5a4e53e51
Vincent Kneip <kneip@zbmed.de>
2024-07-09 09:36:38 +0200

    fix(nowrap): removed nowrap from EntityInfoWidget.tsx, EntityRelationsWidget.tsx

--- src/components/widgets/EntityRelationsWidget/EntityRelationsWidget.tsx
+++ src/components/widgets/EntityRelationsWidget/EntityRelationsWidget.tsx
@@ -353,7 +353,6 @@ function EntityRelationsWidget(props: EntityRelationsWidgetProps) {
             <EuiCard
                 title={hasTitle ? (entityType ? capitalize(getEntityTypeName(entityType)) : (isSuccessEntityRelation && entity) ? capitalize(entity.getType()) : "") + " Relations" : ""}
                 layout="horizontal"
-                style={{whiteSpace: "nowrap"}} // TODO: Does not get displayed correctly on storybook main page
             >
                 {(isLoadingEntityRelation || isLoadingInstances) && <EuiLoadingSpinner size={'s'}/>}
                 {isErrorEntityRelation && <EuiText>Requested resource not available</EuiText>}
