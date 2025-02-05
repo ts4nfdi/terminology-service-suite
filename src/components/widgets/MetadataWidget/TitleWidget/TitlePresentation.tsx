16d6a56b5b29c89fec9d6127e341486cfacf07a2
sasse <sasse@zbmed.de>
2025-02-05 11:40:58 +0100

    fix(TitleWidget): separate logic and presentation

--- src/components/widgets/MetadataWidget/TitleWidget/TitlePresentation.tsx
+++ src/components/widgets/MetadataWidget/TitleWidget/TitlePresentation.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { EuiText } from "@elastic/eui";
+import { EuiLoadingSpinner, EuiText } from "@elastic/eui";
 import {TitlePresentationProps} from "../../../../app/types";
+import { getErrorMessageToDisplay } from "../../../../app/util";
+import "../../../../style/ts4nfdiStyles/ts4nfdiTitleStyle.css"
 
 function TitlePresentation(props: TitlePresentationProps) {
+  const {title, titleText, isLoading, error, defaultValue, className} = props;
+  const finalClassName = className || "ts4nfdi-title-style";
 
-  return (
-    <EuiText className={props.className}>{props.titleText || props.title}</EuiText>
-  );
+  const renderContent = () => {
+    if (titleText) {
+      return <EuiText>{titleText}</EuiText>;
+    }
+
+    if (isLoading) {
+      return defaultValue ?
+          <EuiText>{defaultValue}</EuiText> :
+          <EuiLoadingSpinner size="s" />;
+    }
+
+    if (error) {
+      return defaultValue ?
+          <EuiText>{defaultValue}</EuiText> :
+          <EuiText>{getErrorMessageToDisplay(error, "title")}</EuiText>;
+    }
+
+    return <EuiText>{title}</EuiText>;
+  };
+
+  return <div className={finalClassName}>{renderContent()}</div>;
+
 }
 
 export { TitlePresentation };
