e04a0a460542ca68325710c20569de050616f8a2
jusa3 <sasse@zbmed.de>
2025-07-08 15:46:12 +0200

    build: use separate entry point for server components

--- rollup.config.js
+++ rollup.config.js
@@ -2,35 +2,69 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import copy from "rollup-plugin-copy";
 import commonjs from "@rollup/plugin-commonjs";
+import preserveDirectives from "rollup-plugin-preserve-directives";
+
+const external = [
+  "@elastic/eui",
+  "react",
+  "react-query",
+  "@emotion/react",
+  "react-dom",
+]
+
+const plugins = [
+  postcss({
+    extensions: [".css"],
+    minimize: true,
+    extract: true,
+  })
+]
 
 export default [
-  {
+    {
     input: "src/index.ts",
-    external: [
-      "@elastic/eui",
-      "react",
-      "react-query",
-      "@emotion/react",
-      "react-dom",
-    ],
+    external,
     output: {
       dir: "dist/esm",
       format: "esm",
       sourcemap: true,
+      preserveModules: true,
     },
     plugins: [
-      commonjs({
-        include: ["./index.js", "node_modules/**"],
-      }),
+        ...plugins,
+      typescript({ tsconfig: "./tsconfig.json", outDir: "dist/esm" }),
       copy({
-        targets: [{ src: "src/style/32px.png", dest: "dist/esm" }],
+          targets: [{ src: "src/style/32px.png", dest: "dist/esm" }],
       }),
-      postcss({
-        extensions: [".css"],
-        minimize: true,
-        extract: true,
+      commonjs({
+          include: ["./index.js", "node_modules/**"],
       }),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      preserveDirectives()
+    ],
+    onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+            return;
+        }
+        warn(warning);
+    },
+  },
+  {
+    input: "src/components/server/index.ts",
+    external,
+    output: {
+      dir: "dist/server",
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins: [
+        ...plugins,
+      typescript({ tsconfig: "./tsconfig.json", outDir: "dist/server" }),
+      copy({
+          targets: [{ src: "src/style/32px.png", dest: "dist/server" }],
+      }),
+      commonjs({
+          include: ["./components/server/index.js", "node_modules/**"],
+      }),
     ],
   },
 ];
--- src/components/widgets/MetadataWidget/BreadcrumbWidget/index.ts
+++ src/components/widgets/MetadataWidget/BreadcrumbWidget/index.ts
@@ -1,3 +1,2 @@
 export { BreadcrumbWidget } from "./BreadcrumbWidget";
 export { BreadcrumbPresentation } from "./BreadcrumbPresentation/BreadcrumbPresentation"
-export { getBreadcrumbData } from "./BreadcrumbPresentation/BreadcrumbData"
