b2ccad431b098a165e0e6ae8bfaf52c1d92fb9ec
Vincent Kneip <kneip@zbmed.de>
2025-09-12 14:05:10 +0200

    fix(OntologyBadge): non-clickable if onNavigateToOntology or ontologyId undefined

--- packages/react/src/model/OntologyBadge.tsx
+++ packages/react/src/model/OntologyBadge.tsx
@@ -5,13 +5,18 @@ import {OntologyBadgeProps} from "../app";
 export default function OntologyBadge(props: OntologyBadgeProps): ReactElement {
     return (
         <Badge
-            onClick={() => {
-                if (typeof props.onNavigateToOntology === "function" && props.ontologyId)
-                    props.onNavigateToOntology(props.ontologyId, props.entityType || "", props.iri ? {
-                        iri: props.iri,
-                        label: props.label,
-                    } : undefined);
-            }}
+            onClick={(typeof props.onNavigateToOntology === "function" && props.ontologyId) ?
+                () => {
+                    if (typeof props.onNavigateToOntology === "function" && props.ontologyId)
+                        props.onNavigateToOntology(
+                            props.ontologyId,
+                            props.entityType || "",
+                            props.iri ? {
+                                iri: props.iri,
+                                label: props.label,
+                            } : undefined
+                        );
+                } : undefined}
             color={props.color}
         >
             {props.ontologyId ? props.ontologyId.toUpperCase() : "No ontology name available"}
