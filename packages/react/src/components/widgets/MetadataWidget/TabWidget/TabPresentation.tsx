601e748f41994d4f57b5e814574a943e829bc955
neda <taghizadeh@zbmed.de>
2026-04-22 15:00:16 +0200

    refactor(TabWidget): delete ts4nfdi-tab-container-scroll-fix from css class and move into the component

--- packages/react/src/components/widgets/MetadataWidget/TabWidget/TabPresentation.tsx
+++ packages/react/src/components/widgets/MetadataWidget/TabWidget/TabPresentation.tsx
@@ -285,7 +285,14 @@ function TabPresentation(props: TabPresentationProps) {
       tabs[0];
 
     return (
-      <div className={`${finalClassName} ts4nfdi-tab-container-scroll-fix`}>
+      <div
+        className={finalClassName}
+        style={{
+          display: "grid",
+          gridTemplateColumns: "minmax(0, 1fr)",
+          width: "100%",
+        }}
+      >
         <EuiTabbedContent
           size="s"
           tabs={tabs}
