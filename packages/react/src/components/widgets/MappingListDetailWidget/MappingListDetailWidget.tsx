25199cca78574205a7ec9ee027d1b68066f6446c
neda <taghizadeh@zbmed.de>
2026-05-11 17:30:13 +0200

    feat(MappingListDetailWidget): add baseColumn

--- packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidget.tsx
+++ packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidget.tsx
@@ -1,4 +1,21 @@
+import { EuiBasicTableColumn } from "@elastic/eui";
 import { MappingListDetailWidgetProps } from "../../../app";
+
+type MappingRow = {
+  to: string;
+  toScheme: string;
+  creator: string;
+  type: string;
+  created: string;
+};
+
+const baseColumns: Array<EuiBasicTableColumn<MappingRow>> = [
+  { field: "to", name: "to", truncateText: true, sortable: true },
+  { field: "toScheme", name: "toScheme", truncateText: true, sortable: true },
+  { field: "creator", name: "creator", truncateText: true, sortable: true },
+  { field: "type", name: "type", truncateText: true, sortable: true },
+  { field: "created", name: "created", truncateText: true, sortable: true },
+];
 
 function MappingListDetailWidget(props: MappingListDetailWidgetProps) {
   const { api, iri } = props;
