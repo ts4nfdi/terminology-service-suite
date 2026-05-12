e74d76e8d6ae42f3ffd403112da4a15c6a4b4b49
neda <taghizadeh@zbmed.de>
2026-05-12 11:06:12 +0200

    feat(ColiConcAPI): add ColiConcApi in api file

---
+++ packages/react/src/api/coli-conc/ColiConcAPI.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+export class ColiConcApi {
+  protected axiosInstance: AxiosInstance;
+
+  constructor(api: string | AxiosInstance) {
+    this.axiosInstance =
+      typeof api === "string"
+        ? axios.create({
+            baseURL: api,
+            headers: {
+              Accept: "application/json",
+            },
+          })
+        : api;
+  }
+  protected async makeCall(url: string, config?: AxiosRequestConfig) {
+    const response = await this.axiosInstance.get(url, config);
+
+    return response.data;
+  }
+
+  async getMappingsByFrom(iri: string) {
+    return this.makeCall("", {
+      params: {
+        from: iri,
+      },
+    });
+  }
+}
