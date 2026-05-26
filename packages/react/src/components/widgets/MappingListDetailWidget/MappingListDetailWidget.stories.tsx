f15c8a99669766efc2edd0448b62c9520d647f87
neda <taghizadeh@zbmed.de>
2026-05-26 15:22:55 +0200

    feat(MappingListDetailWidget): add new stories.

--- packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidget.stories.tsx
+++ packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidget.stories.tsx
@@ -2,15 +2,17 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { MappingListDetailDescription } from "../../../app/widgetDescriptions";
 import { WrappedMappingListDetailWidget } from "./MappingListDetailWidget";
 import {
+  MappingListDetailWidgetStoryArgs_B14C4A,
+  MappingListDetailWidgetStoryArgs_Ges_C_2700,
+  MappingListDetailWidgetStoryArgs_Rec_B_2000,
   MappingListDetailWidgetStoryArgTypes,
-  MappingListDetailWidgetStoryArgs,
 } from "./MappingListDetailWidgetStories";
 
 const meta: Meta = {
   title: "Mapping/MappingListDetailWidget",
   component: WrappedMappingListDetailWidget,
   argTypes: MappingListDetailWidgetStoryArgTypes,
-  args: MappingListDetailWidgetStoryArgs,
+  args: MappingListDetailWidgetStoryArgs_Rec_B_2000,
   parameters: {
     layout: "centered",
     docs: {
@@ -25,5 +27,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Rec_B_2000: Story = {
-  args: MappingListDetailWidgetStoryArgs,
+  args: MappingListDetailWidgetStoryArgs_Rec_B_2000,
+};
+
+export const Ges_C_2700: Story = {
+  args: MappingListDetailWidgetStoryArgs_Ges_C_2700,
+};
+
+export const B14C4A: Story = {
+  args: MappingListDetailWidgetStoryArgs_B14C4A,
 };
--- packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidgetStories.ts
+++ packages/react/src/components/widgets/MappingListDetailWidget/MappingListDetailWidgetStories.ts
@@ -1,6 +1,16 @@
 export const MappingListDetailWidgetStoryArgTypes = {};
 
-export const MappingListDetailWidgetStoryArgs = {
+export const MappingListDetailWidgetStoryArgs_Rec_B_2000 = {
   api: "https://coli-conc.gbv.de/api/",
   iri: "http://uri.gbv.de/terminology/nsk/Rec%20B%202000",
+} as const;
+
+export const MappingListDetailWidgetStoryArgs_Ges_C_2700 = {
+  api: "https://coli-conc.gbv.de/api/",
+  iri: "http://uri.gbv.de/terminology/nsk/Ges%20C%202700",
+} as const;
+
+export const MappingListDetailWidgetStoryArgs_B14C4A = {
+  api: "https://coli-conc.gbv.de/api/",
+  iri: "https://www.w3id.org/archlink/terms/conservationthesaurus/B14C4A",
 } as const;
