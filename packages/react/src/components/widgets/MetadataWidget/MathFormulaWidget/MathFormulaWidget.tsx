ae9533b9b29930f3f4607e02b1dbc96e412f6335
pooya.oa <pooya.oladazimi@tib.eu>
2026-06-10 14:12:31 +0200

    feat(MathFormulaWidget): use katext lib

--- packages/react/src/components/widgets/MetadataWidget/MathFormulaWidget/MathFormulaWidget.tsx
+++ packages/react/src/components/widgets/MetadataWidget/MathFormulaWidget/MathFormulaWidget.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { EuiProvider } from "@elastic/eui";
+import "katex/dist/katex.min.css";
+import { BlockMath } from "react-katex";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { MathFormulaWidgetProps } from "../../../../app/types";
 
 function MathFormulaWidget(props: MathFormulaWidgetProps) {
-  void props.content;
+  const mathContent = props.content;
 
-  return <div data-testid="math-formula" />;
+  return (
+    <>
+      <div>
+        <BlockMath math={mathContent} />
+      </div>
+    </>
+  );
 }
 
 function WrappedMathFormulaWidget(props: MathFormulaWidgetProps) {
+  const queryClient = new QueryClient();
   return (
     <EuiProvider colorMode="light" globalStyles={false}>
-      <MathFormulaWidget content={props.content} />
+      <QueryClientProvider client={queryClient}>
+        <MathFormulaWidget content={props.content} />
+      </QueryClientProvider>
     </EuiProvider>
   );
 }
